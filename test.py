ef4989d356995bcdf1a5ae0b20967a46cb8246d5
Will Berman <WLBberman@gmail.com>
2022-10-20 12:57:14 -0700

    fix

--- test.py
+++ test.py
@@ -2,7 +2,9 @@ from diffusers import VQDiffusionPipeline
 
 device = 'cuda'
 
-pipeline = VQDiffusionPipeline.from_pretrained('/content/vq-diffusion-diffusers-dump').to(device)
+pipeline = VQDiffusionPipeline.from_pretrained('/content/vq-diffusion-diffusers-dump')
+
+pipeline = pipeline.to(device)
 
 image = pipeline("horse").images[0]
 
