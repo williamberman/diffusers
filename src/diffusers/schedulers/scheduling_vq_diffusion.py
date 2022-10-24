dcc04f7e6d4a9509773ed5e87d56d7e6791e6c15
William Berman <WLBberman@gmail.com>
2022-10-23 21:57:22 -0700

    generator

--- src/diffusers/schedulers/scheduling_vq_diffusion.py
+++ src/diffusers/schedulers/scheduling_vq_diffusion.py
@@ -49,7 +49,7 @@ def gumbel_noised(logits: torch.FloatTensor, generator: Optional[torch.Generator
     """
     Apply gumbel noise to `logits`
     """
-    uniform = torch.rand_like(logits, device=logits.device, generator=generator)
+    uniform = torch.rand(logits.shape, device=logits.device, generator=generator)
     gumbel_noise = -torch.log(-torch.log(uniform + 1e-30) + 1e-30)
     noised = gumbel_noise + logits
     return noised
