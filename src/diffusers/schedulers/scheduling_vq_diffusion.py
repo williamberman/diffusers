299c05e9680994c0da4eb086bc2ce5bcbd299391
William Berman <WLBberman@gmail.com>
2022-10-23 21:53:24 -0700

    batch

--- src/diffusers/schedulers/scheduling_vq_diffusion.py
+++ src/diffusers/schedulers/scheduling_vq_diffusion.py
@@ -470,12 +470,13 @@ class VQDiffusionScheduler(SchedulerMixin, ConfigMixin):
         return log_Q_t
 
     def apply_cumulative_transitions(self, q, t):
+        bsz = q.shape[0]
         a = self.log_cumprod_at[t]
         b = self.log_cumprod_bt[t]
         c = self.log_cumprod_ct[t]
 
         num_latent_pixels = q.shape[2]
-        c = c.expand(1, 1, num_latent_pixels)
+        c = c.expand(bsz, 1, num_latent_pixels)
 
         q = (q + a).logaddexp(b)
         q = torch.cat((q, c), dim=1)
