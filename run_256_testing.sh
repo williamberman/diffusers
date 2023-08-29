d571124135526aac042166a46ad3f78a2154c2f2
Will Berman <WLBberman@gmail.com>
2023-08-29 23:32:27 +0000

    testing

--- run_256_testing.sh
+++ run_256_testing.sh
@@ -2,8 +2,8 @@ accelerate launch train_t2i_adapter.py \
     --pretrained_model_name_or_path="stabilityai/stable-diffusion-xl-base-1.0" \
     --pretrained_vae_model_name_or_path=madebyollin/sdxl-vae-fp16-fix \
     --mixed_precision=fp16 \
-    --resolution=1024 \
-    --learning_rate=1e-7 \
+    --resolution=256 \
+    --learning_rate=6e-5 \
     --max_train_steps=50000 \
     --max_train_samples=3000000 \
     --dataloader_num_workers=8 \
@@ -12,7 +12,7 @@ accelerate launch train_t2i_adapter.py \
     --train_shards_path_or_url='pipe:aws s3 cp s3://muse-datasets/laion-aesthetic6plus-min512-data/{00000..01210}.tar -' \
     --proportion_empty_prompts=0.05 \
     --validation_steps=100 \
-    --train_batch_size=8 \
+    --train_batch_size=20 \
     --gradient_checkpointing \
     --enable_xformers_memory_efficient_attention \
     --gradient_accumulation_steps=1 \
