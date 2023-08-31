8b80da38e0cd1bbabe9f2825c847cfa5eabbde23
Will Berman <WLBberman@gmail.com>
2023-08-31 01:13:22 +0000

    1024 training script

---
+++ run_1024.sh
@@ -0,0 +1,24 @@
+accelerate launch train_t2i_adapter.py \
+    --pretrained_model_name_or_path="stabilityai/stable-diffusion-xl-base-1.0" \
+    --pretrained_vae_model_name_or_path=madebyollin/sdxl-vae-fp16-fix \
+    --mixed_precision=fp16 \
+    --resolution=1024 \
+    --learning_rate=1e-5 \
+    --max_train_steps=50000 \
+    --max_train_samples=3000000 \
+    --dataloader_num_workers=8 \
+    --validation_image=./t2i_pose_validation \
+    --validation_prompt "spiderman, high quality, 4k" "the dalai lama, high quality, 4k" "elon musk, high quality, 4k" "a lumberjack, high quality 4k" \
+    --train_shards_path_or_url='pipe:aws s3 cp s3://muse-datasets/laion-aesthetic6plus-min512-data/{00000..01210}.tar -' \
+    --proportion_empty_prompts=0.05 \
+    --validation_steps=500 \
+    --train_batch_size=8 \
+    --gradient_checkpointing \
+    --enable_xformers_memory_efficient_attention \
+    --gradient_accumulation_steps=1 \
+    --seed=42 \
+    --report_to=wandb \
+    --use_8bit_adam \
+    --use_euler \
+    --use_ema \
+    --num_validation_images 2
